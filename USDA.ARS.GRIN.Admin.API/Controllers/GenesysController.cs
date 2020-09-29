ce1006af83b19e9d72de79708b3565232da5efb5
Benjamin Haag <Benjamin.Haag@usda.gov>
2020-09-29 16:35:41 -0400

    TEST COMMIT

---
+++ USDA.ARS.GRIN.Admin.API/Controllers/GenesysController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+//using System.Web.Mvc;
+using USDA.ARS.GRIN.Admin.Models;
+using USDA.ARS.GRIN.Admin.Service;
+
+namespace USDA.ARS.GRIN.Admin.API.Controllers
+{
+    [RoutePrefix("api/species")]
+    public class GenesysController : ApiController
+    {
+        [Route]
+        public IHttpActionResult Get()
+        {
+            IQueryable<Species> speciesList = null;
+
+            try
+            {
+                speciesList = _taxonomyService.FindSpecies("malus");
+                return Ok(speciesList);
+            }
+            catch (Exception ex)
+            {
+                // TODO: ADD LOGGING
+                return InternalServerError();
+            }
+        }
+
+    }
+}
